e75530b4d785473444ef9c57959c32813bdb81fe
hodory <mo02@outlook.kr>
2020-03-12 22:03:51 +0900

    한개의 함수에서 여러개의 값을 리턴하기

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func multiply(a, b int) int {
 	return a * b
 }
 
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
 func main() {
+	totalLength, upperName := lenAndUpper("hodory")
+	fmt.Println(totalLength, upperName)
 	fmt.Println(multiply(2, 2))
 }
