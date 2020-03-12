3620db500ed47d8477c60a642b8d333ba10a2b73
hodory <mo02@outlook.kr>
2020-03-12 22:15:17 +0900

    하나의 argument 인자에 여러개의 값을 전달하고자 할 때

--- main.go
+++ main.go
@@ -13,8 +13,14 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
 	totalLength, _ := lenAndUpper("hodory")
 	fmt.Println(totalLength)
 	fmt.Println(multiply(2, 2))
+
+	repeatMe("react", "vue", "angular", "svelte", "backbone")
 }
