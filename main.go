97c1cfde51921ac7eeab454a9b9d9896cf0b1488
hodory <mo02@outlook.kr>
2020-03-13 18:26:18 +0900

    Go function의 naked return

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ func multiply(a, b int) int {
 	return a * b
 }
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+
+	return
 }
 
 func repeatMe(words ...string) {
@@ -18,8 +21,8 @@ func repeatMe(words ...string) {
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("hodory")
-	fmt.Println(totalLength)
+	totalLength, upper := lenAndUpper("hodory")
+	fmt.Println(totalLength, upper)
 	fmt.Println(multiply(2, 2))
 
 	repeatMe("react", "vue", "angular", "svelte", "backbone")
