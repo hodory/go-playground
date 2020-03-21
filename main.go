6709df5d13089ee58739822ac9d771566ba985d4
hodory <mo02@outlook.kr>
2020-03-21 21:13:14 +0900

    Update를 추가해보자

--- main.go
+++ main.go
@@ -7,20 +7,14 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	err := dictionary.Update(baseWord, "Second")
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition: ", hello)
-
-	err2 := dictionary.Add(word, definition)
-
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word already exist")
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Can't update non-existing word")
+	errWordExists = errors.New("That word already exist")
+)
 
 // Seach for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -25,5 +28,18 @@ func (d Dictionary) Add(word, def string) error {
 	} else if err == nil {
 		return errWordExists
 	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+
 	return nil
 }
