3c8315272c83f441bd88aa9e221ff70d3b424c3e
hodory <mo02@outlook.kr>
2020-03-21 21:07:24 +0900

    Add Method를 만들어보자

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("second")
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition: ", hello)
+
+	err2 := dictionary.Add(word, definition)
+
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,7 +5,8 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errorNotFound = errors.New("Not Found")
+var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exist")
 
 // Seach for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -13,5 +14,16 @@ func (d Dictionary) Search(word string) (string, error) {
 	if exists {
 		return value, nil
 	}
-	return "", errorNotFound
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil
 }
