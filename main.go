614e1b6f97659194669278741cd8f6334a1f7a07
hodory <mo02@outlook.kr>
2020-03-21 20:59:45 +0900

    Map의 Value를 리턴하는 함수를 Dictionary에 선언하고 사용해보자

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hodory/go-playground/mydict"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{}
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("second")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 }
