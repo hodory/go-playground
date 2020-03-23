ff19884d81cd8596523876f5bf6e68a12f79cd97
hodory <mo02@outlook.kr>
2020-03-23 23:25:27 +0900

    My first Go routine

--- main.go
+++ main.go
@@ -1,45 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request Failed")
-
 func main() {
-	var results = map[string]string{}
-
-	urls := []string{
-		"https://www.airbnb.com",
-		"https://www.google.co.kr",
-		"https://www.naver.com",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitUrl(url)
-
-		if err != nil {
-			result = "FAILED"
-		}
-
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go printText("Hello")
+	printText("World")
 }
 
-func hitUrl(url string) error {
-	fmt.Println("Checking Url : ", url)
-	res, err := http.Get(url)
-
-	if err != nil || res.StatusCode >= 400 {
-		return errRequestFailed
+func printText(text string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(text)
+		time.Sleep(time.Second)
 	}
-
-	return nil
 }
