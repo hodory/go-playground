18339f0a770eeb9b6d9f0e98eaed8c0429a569c2
hodory <mo02@outlook.kr>
2020-03-28 16:46:30 +0900

    채용공고의 id, 제목, 기업 주소를 받아오자

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseUrl = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
@@ -22,6 +30,25 @@ func getPage(page int) {
 	pageUrl := baseUrl + "&start=" + strconv.Itoa(page*50)
 
 	fmt.Println("Requesting : ", pageUrl)
+
+	res, err := http.Get(pageUrl)
+
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find(".title>a").Text()
+		location := card.Find(".sjcl").Text()
+		fmt.Println(id, title, location)
+	})
 }
 
 func getPages() int {
